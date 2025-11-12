ab96573836ab26d84c815d975982a47714f3eb43
Manii753 <110933628+Manii753@users.noreply.github.com>
2025-11-12 15:26:49 +0500

    session problem v2

--- src/app/api/user/clickup-token/route.js
+++ src/app/api/user/clickup-token/route.js
@@ -33,7 +33,6 @@ export async function POST(request) {
       return NextResponse.json({ error: 'User not found' }, { status: 404 });
     }
     
-    console.log("user after adding accesstken",user);
     return NextResponse.json({ message: 'ClickUp access token added successfully', user: user });
     
   } catch (error) {
